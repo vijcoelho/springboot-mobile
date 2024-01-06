b0a3874fd9f9ad7549e3d722331de9d04d5971a6
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2024-01-06 18:05:28 -0300

    up

--- SpringServer/src/main/java/com/api/SpringServer/model/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.SpringServer.model.employee;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-
-}
---
+++ springserver/src/main/java/com/api/springserver/SpringserverApplication.java
@@ -0,0 +1,12 @@
+package com.api.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringserverApplication.class, args);
+	}
+}
