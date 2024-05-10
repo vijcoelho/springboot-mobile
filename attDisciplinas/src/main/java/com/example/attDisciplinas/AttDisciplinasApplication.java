b7432a740a9b2ea88db84bef059ff74ca9535a07
Vitor Coelho <vijcoelho@gmail.com>
2024-05-10 18:39:03 -0300

    up

---
+++ attDisciplinas/src/main/java/com/example/attDisciplinas/AttDisciplinasApplication.java
@@ -0,0 +1,13 @@
+package com.example.attDisciplinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttDisciplinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttDisciplinasApplication.class, args);
+	}
+
+}
