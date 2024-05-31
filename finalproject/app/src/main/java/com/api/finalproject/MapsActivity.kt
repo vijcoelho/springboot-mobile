d66348054d0aa2304374efe12dc2a230d2427efd
Vitor Coelho <vijcoelho@gmail.com>
2024-05-30 22:42:23 -0300

    up

---
+++ finalproject/app/src/main/java/com/api/finalproject/MapsActivity.kt
@@ -0,0 +1,37 @@
+package com.api.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.api.finalproject.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val cotuca = LatLng(-22.90212878220126, -47.066942787938615)
+        mMap.addMarker(MarkerOptions().position(cotuca).title("Cotuca"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cotuca))
+    }
+}
