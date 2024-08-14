99e647b09819f212edc14e0145032bba2db856cd
Vitor Coelho <vijcoelho@gmail.com>
2024-08-13 22:35:23 -0300

    up

---
+++ aula_01/aula_01/aula_01App.swift
@@ -0,0 +1,17 @@
+//
+//  aula_01App.swift
+//  aula_01
+//
+//  Created by VITOR JOSE COELHO on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct aula_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
